008005618900895138ea35e6e1d70075ac275fe7
zabe-dev <jay@zabe.dev>
2026-09-08 08:13:19 +0800

    Update favicons

--- components/ui/brand.tsx
+++ components/ui/brand.tsx
@@ -5,7 +5,7 @@ export function Brand() {
   return (
     <Link className="brand" href="/" aria-label="EndorseCoin home">
       <span className="brand-mark">
-        <Image src="/logo.png" alt="EndorseCoin logo" width={40} height={40} />
+        <Image src="/logo.svg" alt="EndorseCoin logo" width={40} height={40} />
       </span>
       <span>endorsecoin</span>
     </Link>
